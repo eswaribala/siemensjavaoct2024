103d86844718fb6eac8ece6d792168388efad779
Parameswari Bala <parameswaribala@gmail.com>
2024-10-16 12:22:54 +0530

    updated

--- webstorespringcoreannotation/src/main/java/com/siemens/webstore/Main.java
+++ webstorespringcoreannotation/src/main/java/com/siemens/webstore/Main.java
@@ -5,38 +5,17 @@ import com.siemens.webstore.models.CreditCard;
 import com.siemens.webstore.models.SilverCreditCard;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
 public class Main {
     public static void main(String[] args) {
 
-      //IOC
-        //step1
-        Resource resource= new ClassPathResource("webstore-config.xml");
-        //step2
-        //container
-        DefaultListableBeanFactory defaultListableBeanFactory=new DefaultListableBeanFactory();
-        XmlBeanDefinitionReader xmlBeanDefinitionReader=new XmlBeanDefinitionReader
-                (defaultListableBeanFactory);
-        xmlBeanDefinitionReader.loadBeanDefinitions(resource);
-        //step3
-        //Implementing IOC
-        Catalog catalog1= (Catalog) defaultListableBeanFactory.getBean("catalog");
-        //After DI
-        System.out.println(catalog1);
+        ApplicationContext applicationContext=new
+                ClassPathXmlApplicationContext("webstore-config.xml");
 
-        //Singleton
-        Catalog catalog2= (Catalog) defaultListableBeanFactory.getBean("catalog");
 
-       catalog2.setName("Furniture");
-       catalog2.setDescription("Home Furniture");
-        System.out.println(catalog1);
-
-        //silver card
-        SilverCreditCard silverCreditCard= (SilverCreditCard) defaultListableBeanFactory.getBean("silverCreditCard");
-        System.out.println(silverCreditCard.getCreditCardNo()
-                +","+silverCreditCard.getCvv()+","
-                +silverCreditCard.getUsageLimit());
     }
 }
--- webstorespringcoreannotation/src/main/java/com/siemens/webstore/models/Catalog.java
+++ webstorespringcoreannotation/src/main/java/com/siemens/webstore/models/Catalog.java
@@ -3,6 +3,7 @@ package com.siemens.webstore.models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,4 +17,8 @@ public class Catalog{
     private String description;
     //has relationship
     private List<Category> categoryList;
+    @Autowired
+    public Catalog(List<Category> categoryList) {
+        this.categoryList = categoryList;
+    }
 }
