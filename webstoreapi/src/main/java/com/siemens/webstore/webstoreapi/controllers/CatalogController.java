71842b703a00ca57390969ba4326be6e30b52db3
Parameswari Bala <parameswaribala@gmail.com>
2024-10-17 14:28:13 +0530

    updated

--- webstoreapi/src/main/java/com/siemens/webstore/webstoreapi/controllers/CatalogController.java
+++ webstoreapi/src/main/java/com/siemens/webstore/webstoreapi/controllers/CatalogController.java
@@ -39,5 +39,47 @@ public class CatalogController {
     }
 
 
+    @GetMapping("/v1.0/{catalogId}")
+    public ResponseEntity<ResponseWrapper> findCatalogById(@PathVariable("catalogId") long catalogId){
+
+        Catalog catalogInstance=this.catalogService.findCatalogById(catalogId);
+        if(catalogInstance!=null)
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(new ResponseWrapper(catalogInstance));
+        else
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(new ResponseWrapper("Catalog Instance not available"));
+
+
+    }
+
+    @PutMapping("/v1.0")
+    public ResponseEntity<ResponseWrapper> updateCatalogById(@RequestParam("catalogId") long catalogId,
+                                                             @RequestParam("catalogName") String catalogName){
+
+        Catalog catalogInstance=this.catalogService.updateCatalog(catalogId,catalogName);
+        if(catalogInstance!=null)
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(new ResponseWrapper(catalogInstance));
+        else
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(new ResponseWrapper("Catalog Instance Could not be updated because of invalid catalog id"));
+
+
+    }
+
+    @DeleteMapping("/v1.0")
+    public ResponseEntity<ResponseWrapper> deleteCatalogById(@RequestParam("catalogId") long catalogId){
+
+
+        if(this.catalogService.deleteCatalog(catalogId))
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(new ResponseWrapper("Catalog Id="+catalogId+"deleted successfully"));
+        else
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(new ResponseWrapper("Catalog Id="+catalogId+"Could not be found"));
+
+
+    }
 
 }
