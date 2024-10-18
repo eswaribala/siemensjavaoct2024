78ab4b6036cbbf59e6ad1a18778af00af30d4373
Parameswari Bala <parameswaribala@gmail.com>
2024-10-18 14:14:49 +0530

    updated

---
+++ webstoreapijwt/src/main/java/com/siemens/webstore/webstoreapi/configurations/ApiSecurityConfig.java
@@ -0,0 +1,100 @@
+package com.siemens.webstore.webstoreapi.configurations;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.RegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+@Configuration
+//@EnableWebSecurity
+//spring boot 3 onwards
+@EnableMethodSecurity
+public class ApiSecurityConfig  {
+
+	/*@Autowired
+	private UserAuthService userAuthService;
+
+	@Autowired
+	private JwtAuthenticationFilter jwtAuthenticationFilter;*/
+
+	@Autowired
+	private ApiAuthenticationEntryPoint authenticationEntryPoint;
+
+	
+	
+	
+
+
+	/*@Autowired
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userAuthService).passwordEncoder(passwordEncoder());
+	}*/
+	
+	@Bean
+	 public WebSecurityCustomizer ignoringCustomizer() {
+	        return (web) -> web.ignoring().requestMatchers( "/users/v1.0/signup","/v3/api-docs/**",
+					"/swagger-ui/**",
+					"/swagger-ui.html");
+	 }
+
+	 @Bean
+	  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		 http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(authz -> authz
+				 .requestMatchers("/v3/api-docs/**",
+						 "/swagger-ui/**",
+						 "/swagger-ui.html", "/users/v1.0/signup")
+
+				 
+		 .permitAll().anyRequest()
+			.authenticated())
+		 
+		 .exceptionHandling((exception)-> 
+		 exception.authenticationEntryPoint(authenticationEntryPoint))
+		 .sessionManagement(sess -> sess.sessionCreationPolicy
+				 (SessionCreationPolicy.STATELESS))  ;
+		 
+//	     .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+		      return http.build();
+	   }
+
+
+
+	//@Bean
+	/*public RegistrationBean jwtAuthFilterRegister(JwtAuthenticationFilter filter) {
+		FilterRegistrationBean<JwtAuthenticationFilter> registrationBean = new FilterRegistrationBean<>(filter);
+		registrationBean.setEnabled(false);
+		return registrationBean;
+	}
+*/
+	 @Bean
+     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+         return authenticationConfiguration.getAuthenticationManager();
+     }
+ 
+	
+		/*
+		 * @Bean public AuthenticationManager authenticationManagerBean() throws
+		 * Exception { return super.authenticationManagerBean(); }
+		 */
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+}
