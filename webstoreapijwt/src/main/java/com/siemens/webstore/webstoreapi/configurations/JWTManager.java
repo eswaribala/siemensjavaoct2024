234217da092b13764274507be4cb31e32daffdda
Parameswari Bala <parameswaribala@gmail.com>
2024-10-18 15:04:04 +0530

    updated

---
+++ webstoreapijwt/src/main/java/com/siemens/webstore/webstoreapi/configurations/JWTManager.java
@@ -0,0 +1,27 @@
+package com.siemens.webstore.webstoreapi.configurations;
+
+import com.siemens.webstore.webstoreapi.models.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.Date;
+
+public class JWTManager {
+
+    @Value("${jwt.secretkey}")
+    private String secretKey;
+    @Value("${jwt.token.validity}")
+    private long expiryTime;
+
+    public String generateToken(User user){
+        Claims claims = Jwts.claims().setSubject(user.getUserName());
+        claims.put("roles", user.getRoles());
+        long nowMillis = System.currentTimeMillis();
+        long expMillis = nowMillis + expiryTime;
+        Date exp = new Date(expMillis);
+        return Jwts.builder().setClaims(claims).setIssuedAt(new Date(nowMillis)).setExpiration(exp)
+                .signWith(SignatureAlgorithm.HS512, secretKey).compact();}
+
+}
